d0498f83606cd63efad04d2e22bcf0d528d1f23f
Gavrilko Sergei <seregagavrilko@mail.ru>
2017-06-06 05:13:30 +0300

    first commit

---
+++ js/my.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sergeigavrilko on 31.05.17.
+ */
+$(document).ready(function(){
+    $('.slick-csm').slick({
+        dots: true,
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 10000
+    });
+});
